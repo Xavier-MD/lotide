f1639099a6fef686d3936bdd3c5522d86e5b8f65
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-09-14 22:22:16 -0400

    Formatting edits.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,4 @@
 const eqArrays = function(array1, array2) {
-
   if (array1.length >= array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
@@ -18,7 +17,6 @@ const eqArrays = function(array1, array2) {
 };
 
 const assertArraysEqual = function(array1, array2) {
-
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else if (!eqArrays(array1, array2)) {
