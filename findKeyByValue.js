0b547b3c622b63f0b424c0f7b49f5c49845b4942
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-09-16 15:42:08 -0400

    Created a function that searches for a key on an object where its value matches a given value.

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  anime: "Attack On Titan",
+  cartoon: "Fairly Odd Parents"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Fairly Odd Parents"), "cartoon");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Attack On Titan"), "anime");
