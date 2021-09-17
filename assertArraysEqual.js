c768fd9ad5089a477575d0824a49552ce8ce2163
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-09-16 21:03:51 -0400

    Changed the value of array1 to actual and the value of array2 to expected for continuity with other similar functions.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,11 +16,11 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else if (!eqArrays(array1, array2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
