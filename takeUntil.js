b5b0a8be3c0322a46d03643301553fbe5359bdde
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-09-18 00:29:04 -0400

    Created a function that returns a slice of the array with elements taken from the beginning. It keeps going until the callback returns a truthy value.

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+const takeUntil = function(array, callback) {
+  let newArray = [];
+  
+  for (let element of array) {
+    if (!callback(element)) {
+      newArray.push(element);
+    } else {
+      return newArray;
+    }
+  }
+  return newArray;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const eqArrays = function(array1, array2) {
+  if (array1.length >= array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else if (array1.length < array2.length) {
+    for (let i = 0; i < array2.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual(takeUntil([1, 2, 3, 4, 5, 6, 7], x => x > 4), [1, 2, 3, 4]);
